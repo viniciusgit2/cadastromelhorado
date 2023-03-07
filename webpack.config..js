b40907974bea27aa1a2f9a0b4606ee8478124809
viniciusgit2 <4xv6patelfin@gmail.com>
2023-03-07 09:41:36 -0300

    pequenas alteracoes

---
+++ webpack.config..js
@@ -0,0 +1,89 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+const path = require(`path`);
+import { dirname, join } from path;
+const webpack=require (webpack)
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
+const include = join(__dirname, "src")
+const isProduction = process.env.NODE_ENV == "production";
+
+const stylesHandler = MiniCssExtractPlugin.loader;
+export default{
+  context:path.resolve (dirname,"./src"),
+   entry: `../src/spote/main.js`,
+  output:{
+    path:join(__dirname, `dist`),
+    librarytarget:`umd`,
+    library:`spote`,
+    filename:"bundle.js"
+  }, devServer:{
+    contentbase:path.resolve(dirname,"dist")
+  },
+  devtool:`source-map`,
+    open: true,
+    host: "localhost",
+  module:{
+    loaders:[{test: /\.js$/, loader:`babel-loader`,include }]
+    
+  }
+
+  
+}
+const config = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    open: true,
+    host: "localhost",
+    contentbase: path.resolve(__dirname,"dist")
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
+
+    new MiniCssExtractPlugin(),
+
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: [stylesHandler, "css-loader"],
+      },
+      {
+        test: /\.styl$/i,
+        use: [stylesHandler, "css-loader", "stylus-loader"],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset",
+      }
+
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
+    ],
+  }
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "production";
+
+    config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
+  } else {
+    config.mode = "development";
+  }
+  return config;
+}
+
+
